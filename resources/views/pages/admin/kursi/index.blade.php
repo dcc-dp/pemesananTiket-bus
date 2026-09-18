cad14c9315f9f6ad780643b3e0aec0f67c7a023f
rahmanhia <rahmanhiarmdhni@gmail.com>
2026-09-18 13:08:37 +0800

    feat:kursi(admin

--- resources/views/pages/admin/kursi/index.blade.php
+++ resources/views/pages/admin/kursi/index.blade.php
@@ -50,7 +50,7 @@
                                     <th>No</th>
                                     <th>Nomor Kursi</th>
                                     <th>Kelas</th>
-                                    <th>Harga</th>
+                                    <th>Harg a</th>
                                     <th>Posisi</th>
                                     <th>Status</th>
                                     <th class="text-right">Aksi</th>
